70ac7117793bfe16dee1c7cd1f8a2a3392cedf37
Ringo De Smet <ringo@de-smet.name>
2025-03-22 20:43:38 +0100

    Upgrade to Pulumi Scaleway v1.25.0

--- hardes/services.go
+++ hardes/services.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+
 	scw "github.com/pulumiverse/pulumi-scaleway/sdk/go/scaleway"
 )
 
 func NewServicesProject(ctx *pulumi.Context, org string) error {
+	// Create the Scaleway Project for the services we will run on Kubernetes
 	servicesProjectName := "hardes-services"
 	_, err := scw.NewAccountProject(ctx, servicesProjectName, &scw.AccountProjectArgs{
 		Name:           pulumi.String(servicesProjectName),
@@ -17,5 +19,14 @@ func NewServicesProject(ctx *pulumi.Context, org string) error {
 	if err != nil {
 		return fmt.Errorf("error creating project %v: %v", servicesProjectName, err)
 	}
+
+	// Create an IAM Group
+
+	// Create some policies with restricted access to this project and the group as principal
+
+	// Create an IAM Application and API keys. Add the Application to the Group
+
+	// Create a Pulumi ESC environment exposing the Application API credentials
+
 	return nil
 }
