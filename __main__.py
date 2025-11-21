f2e5ab5562aff7f41b835c47c2f10725c937cc14
Ringo De Smet <ringo@de-smet.name>
2025-11-21 09:06:45 +0100

    Renamed & refactored the stack.

--- __main__.py
+++ __main__.py
@@ -1,58 +1,73 @@
 import pulumi
 import pulumiverse_scaleway as scaleway
 from pulumiverse_scaleway.iam import PolicyRuleArgs
-import pulumi_pulumiservice as puservice
+import pulumi_pulumiservice as pulumiservice
+
+from pydantic import BaseModel
+
+class ProjectInfo(BaseModel):
+    name: str
+    description: str
 
 config = pulumi.Config()
 
-project_name = "hardes"
-organization_id = config.require_secret("organizationId")
+# From ESC environment
+scw_organization_id = config.require_secret("organizationId")
 
+# From stack local config
+project_info = ProjectInfo(**config.require_object("project"))
+
+# Scaleway project
 project = scaleway.account.Project(
-    project_name,
-    name=project_name,
-    description="Hardes",
-    organization_id=organization_id,
+    "scaleway-project",
+    name=project_info.name,
+    description=project_info.description,
+    organization_id=scw_organization_id,
 )
 
+# Scaleway Admin Group
 admin_group = scaleway.iam.Group(
-    f"{project_name}-dns",
-    organization_id=organization_id,
+    "admin-group",
+    name=f"{project_info.name}-administrators",
+    organization_id=scw_organization_id,
 )
 
 policy = scaleway.iam.Policy(
-    f"{project_name}-admin",
-    name=f"{project_name}-admin",
+    "policy-admin",
+    name=f"{project_info.name}-admin",
     group_id=admin_group.id,
-    organization_id=organization_id,
+    organization_id=scw_organization_id,
     rules=[
         PolicyRuleArgs(
-            organization_id=organization_id,
+            organization_id=scw_organization_id,
             permission_set_names=["IAMManager"]
         ),
         PolicyRuleArgs(
             project_ids=[project.id],
             permission_set_names=["AllProductsFullAccess"]
         ),
-    ])
+    ],
+)
 
 app = scaleway.iam.Application(
-    "pulumi-hardes",
-    organization_id=organization_id,
+    "app-pulumi",
+    name=f"{project_info.name}-pulumi",
+    organization_id=scw_organization_id,
 )
 
 membership = scaleway.iam.GroupMembership(
-    "pulumi-hardes-member",
+    "admin-group-member-app-pulumi",
     group_id=admin_group.id,
     application_id=app.id,
 )
 
 api_key = scaleway.iam.ApiKey(
-    "pulumi-hardes",
+    "apikey-pulumi",
+    description=f"Pulumi API Key for {project_info.name}-pulumi",
     application_id=app.id,
 )
 
-hardesEscEnvironment = puservice.Environment(
+escEnvironment = pulumiservice.Environment(
     "environmentResource",
     name="hardes",
     organization=pulumi.get_organization(),
@@ -60,7 +75,7 @@ hardesEscEnvironment = puservice.Environment(
     project="scaleway"
 )
 
-pulumi.export("organizationId", organization_id)
+pulumi.export("organizationId", scw_organization_id)
 pulumi.export("projectId", project.id)
 pulumi.export("accessKey", api_key.access_key)
 pulumi.export("secretKey", api_key.secret_key)
